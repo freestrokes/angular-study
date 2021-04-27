c46cbe38d5fbf57e82c48c8039ead605445c8c8d
Harry <exntu-metatron@googlegroups.com>
2021-04-27 15:13:52 +0900

    #master Add comment out for unused code.

--- angular-frontend/src/main.server.ts
+++ angular-frontend/src/main.server.ts
@@ -1,10 +1,11 @@
-import { enableProdMode } from '@angular/core';
-
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-export { AppServerModule } from './app/app.server.module';
-export { renderModule, renderModuleFactory } from '@angular/platform-server';
+//TODO
+// import { enableProdMode } from '@angular/core';
+//
+// import { environment } from './environments/environment';
+//
+// if (environment.production) {
+//   enableProdMode();
+// }
+//
+// export { AppServerModule } from './app/app.server.module';
+// export { renderModule, renderModuleFactory } from '@angular/platform-server';
