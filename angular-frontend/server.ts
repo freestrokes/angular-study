f2c81914916cf357b49624f5c9144d04c56cf0f5
Harry <exntu-metatron@googlegroups.com>
2021-05-14 16:05:02 +0900

    #master Added server setting for heroku.

--- angular-frontend/server.ts
+++ angular-frontend/server.ts
@@ -75,3 +75,20 @@
 // }
 //
 // export * from './src/main.server';
+
+//TODO
+// Set path after run command 'npm run-script build'
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/angular-study/browser'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/angular-study/browser/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
