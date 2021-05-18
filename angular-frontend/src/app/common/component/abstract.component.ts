1d716f2a710dd76db6e7013c54e4f793fbc757fc
freestrokes <freestrokes.hk@gmail.com>
2021-05-18 18:51:36 +0900

    #master Added abstract component.

---
+++ angular-frontend/src/app/common/component/abstract.component.ts
@@ -0,0 +1,26 @@
+import {ChangeDetectorRef, ElementRef, Injector, OnDestroy, OnInit} from '@angular/core';
+
+export abstract class AbstractComponent implements OnInit, OnDestroy {
+
+  // Change Detect
+  protected changeDetect: ChangeDetectorRef;
+
+  protected constructor(protected elementRef: ElementRef,
+                        protected injector: Injector) {
+    this.changeDetect = injector.get(ChangeDetectorRef);
+  }
+
+  public ngOnInit(): void {
+  }
+
+  public ngOnDestroy(): void {
+  }
+
+  protected safelyDetectChanges(): void {
+    // tslint:disable-next-line:no-string-literal
+    if (!this.changeDetect['destroyed']) {
+      this.changeDetect.detectChanges();
+    }
+  } // func - safelyDetectChanges
+
+}
